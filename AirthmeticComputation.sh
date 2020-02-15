29ce7c39ee641f7d32bfd11438225c2ed2e7c6e3
kishlayswapnil <kishlayswapnil@gmail.com>
2020-02-15 18:32:25 +0530

    Added: Added another operation to compute multiplication and addition.

--- AirthmeticComputation.sh
+++ AirthmeticComputation.sh
@@ -6,5 +6,8 @@ read -p "Enter the first number: " firstNumber
 read -p "Enter the second number: " secondNumber
 read -p "Enter the third number: " thirdNumber
 
-result=$((firstNumber+secondNumber*thirdNumber))
-	echo "$firstNumber + $secondNumber * $thirdNumber = $result"
+result1=$((firstNumber+secondNumber*thirdNumber))
+	echo "$firstNumber + $secondNumber * $thirdNumber = $result1"
+result2=$((firstNumber*secondNumber+thirdNumber))
+   echo "$firstNumber * $secondNumber + $thirdNumber = $result2"
+
